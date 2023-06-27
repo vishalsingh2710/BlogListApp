4e0f94204bbb9b779bd60544750c8ffcf8d177c5
vishalsingh2710 <rvishal2710@gmail.com>
2023-06-27 19:17:51 +0000

    initial message

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="profile-img"
+    />
+    <h1 className="user-name"> Wade Warren</h1>
+    <p className="user-designation"> Software developer at UK</p>
+  </div>
+)
+
+export default UserInfo
